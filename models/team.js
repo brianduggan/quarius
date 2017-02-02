7b2638dd6b81dcc244a184c936a60990dda637d2
Brian Duggan <bduggan3387@gmail.com>
2017-02-02 14:34:36 -0500

    fixes primary and secondary color display styles on home page

--- models/team.js
+++ models/team.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var TeamSchema = mongoose.Schema({
   name: {type: String, required: true},
   management: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-  teamMembers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
+  teamMembers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  active: {type: Number, required: true}
 }, {timestamps: true});
 
 module.exports = mongoose.model('Team', TeamSchema);
--- public/js/controllers/teamsController.js
+++ public/js/controllers/teamsController.js
@@ -4,35 +4,40 @@ teamsController.controller('TeamsController', ['$scope', '$http', '$cookies', '$
   $scope.team = {
     name: '',
     management: [],
-    teamMembers: []
+    teamMembers: [],
+    active: 1
   }
 
   $scope.currentTeam = '';
 
   $scope.createTeam = function(){
     var team = $scope.team;
+    // ADDS CREATOR TO BOTH TEAM AND MANAGEMENT SECTIONS
     team.management.push($scope.currentUser._id);
     team.teamMembers.push($scope.currentUser._id);
+    //REQUEST TO CREATE TEAM
     $http.post('/teams', team).then(function(response){
       console.log(response);
       var teamID = response.data._id;
-      $scope.currentUser.teams.push(teamID);
-      $scope.teamManagement();
-      $scope.getCurrentTeam(teamID)
+      $scope.currentUser.teams.push(teamID); //ADDS TEAM ID TO CURRENT USER'S TEAMS
+      $scope.teamManagement(); //NAVIGATES TO TEAM MANAGEMENT VIEW... THIS LINE MAYBE REDUNDANT
+      $scope.getCurrentTeam(teamID); //LOADS CURRENT TEAM
+      // REQUEST TO SAVE USER WITH NEW TEAM ID
       $http.patch('/users/'+ $scope.currentUser._id, $scope.currentUser).then(function(newRes){
         $scope.getCurrentUser();
       })
     })
   }
 
+
   $scope.getCurrentTeam = function(id){
     $http.get('/teams/' + id).then(function(response){
       $scope.currentTeam = response.data;
       $scope.teamManagement();
-      console.log($scope.currentTeam);
     })
   }
 
+  //FILTERS VIEW TO SEE IF CURRENT USER IS A MANAGER OF A GIVEN TEAM
   $scope.filterForManagement = function(){
     for (var i = 0; i < $scope.currentTeam.management.length; i++) {
       if($scope.currentTeam.management[i]._id === $scope.currentUser._id){
@@ -42,26 +47,26 @@ teamsController.controller('TeamsController', ['$scope', '$http', '$cookies', '$
     return false;
   }
 
+  //FILTERS SEARCH RESULTS TO ONLY VIEW ADD TEAM MEMBER OPTION IF A USER IS NOT CURRENTLY A TEAM MEMBER
   $scope.searchNewMembers = function(id){
     for (var i = 0; i < $scope.currentTeam.teamMembers.length; i++) {
       if ($scope.currentTeam.teamMembers[i]._id === id){
         return false;
       }
     }
-    return true
+    return true;
   }
 
+  //ADDS A USER FROM THE SEARCH LIST TO THE CURRENT TEAM
   $scope.addToCurrentTeam = function(userID){
-    console.log(userID);
-    console.log('Hi!');
     var teamID = $scope.currentTeam._id;
     if( !$scope.currentTeam.teamMembers.includes(userID) ){
-      console.log('wasn\'t there yet');
       $scope.currentTeam.teamMembers.push(userID);
-      console.log($scope.currentTeam);
+      //REQUEST TO UPDATE TEAM WITH NEW USER ID
       $http.put('/teams/' + teamID, $scope.currentTeam).then(function(res1){
         console.log(res1);
         teamID = {teamID: teamID}
+        //REQUEST TO ADD THE CURRENT TEAM TO THE USER'S TEAM ARRAY... DONE ON BACK END
         $http.put('/users/teams/' + userID, teamID).then(function(res2){
           console.log(res2);
           $scope.teamManagement();
